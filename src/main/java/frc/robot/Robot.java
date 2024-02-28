6194ecaf79932b79f9d5394d7c3c315b5223b621
Fumbledblades <145873959+Fumbledblades@users.noreply.github.com>
2024-02-27 18:59:43 -0800

    Omar update xd
    added legit everything

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,241 +1,316 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import com.revrobotics.CANSparkBase.ControlType;
 
-import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.SparkMaxAbsoluteEncoder;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.IdleMode;
+
+import javax.swing.JPopupMenu.Separator;
+
+//import java.sql.DriverAction;
+
 import com.revrobotics.AbsoluteEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-
-
-
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
- * directory.
- */
-    
-
-
-
 public class Robot extends TimedRobot {
 
-  
-  private static final String kDefaultAuto = "out of coummunity";
-  private static final String kCustomAuto = "charge station";
+  private static final String kDefaultAuto = "go straight ";
+  private static final String kCustomAuto = "shotspeaker";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private DifferentialDrive Helios;
 
-  CANSparkMax m_leftLeadDriveMotor;
-  CANSparkMax m_rightLeadDriveMotor;
-  CANSparkMax m_leftFollowDriveMotor;
-  CANSparkMax m_rightFollowDriveMotor;
+  //drive----------------
+  CANSparkMax M_L1;
+  CANSparkMax M_R1;
+  CANSparkMax M_L2;
+  CANSparkMax M_R2;
+  //----------------------
 
-DoubleSolenoid m_clawSolenoid;
- 
-  int kClawForward = 0;
-  int kClawReverse = 1;
+  //rollers-----------------
+  CANSparkMax M_FLNCH;
+  CANSparkMax M_BLNCH;
+  int offset = 0;
+  int SUCk =190;
+  int low = 250;
+  int psh = 200;
+  //------------------------
 
-  CANSparkMax m_ArmSwivelMotor;
- int kArmPosConversion = 5;
- int kArmVelConversion = kArmPosConversion/60;
- int kArmOffset = 100; //degrees
- int kArmStow = 32; //degrees
- int kArmHoriz = 260; //degrees
- int kArmGround = 310; //degrees
- int kArmSlope = 230; //degrees
- int kArmDub = 110; //degrees
- int kArmMaxAngle = 315; //degrees
- int kArmMinAngle = 30; //degrees
+  // scooper postitions------------------------------
+  CANSparkMax M_ARM;
+  CANSparkMax M_ARM2;
+  int kArmPosConversion = 5;
+  int kArmVelConversion = kArmPosConversion / 60;
+  int kArmOffset = 100; // degrees
+  int STW = 0; // degrees
+  int SCOOP = 100; // degrees
+  int AMP = 310;
+  int Speeker = 100; // degrees
+  int PULL = 310; // degrees
+  int kArmMaxAngle = 360; // degrees
+  int kArmMinAngle = 20; // degrees
+  // -------------------------------------------
+
+  int L1Id = 1;
+  int R1Id = 2;
+  int L2Id = 3;
+  int R2Id = 4;
+  int ARMId = 5;
+  int ARM2Id =6;
+  int FRLLId = 7;
+  int BRLLId = 8;
+
+  int CurrentLimit = 50;
+
+  //PID------------------------
+
+  AbsoluteEncoder m_armEncoder;
+  AbsoluteEncoder backroll;
+  AbsoluteEncoder frontroll;
+  
+  SparkPIDController m_armPIDController;
+  SparkPIDController Frontrollpid;
+  SparkPIDController Backrollpid;
 
   double Position = 0.05;
   double Iposition = 0.05;
   double Dposition = 0.005;
   double FF = 0.05;
-
-  private DifferentialDrive m_myRobot;
-
- private XboxController driveController = new XboxController(1);
- private XboxController opController = new XboxController(0);
-  
-  int kLeftLeadMotorID = 1;
-  int kRightLeadMotorID = 2;
-  int kLeftFollowMotorID = 3;
-  int kRightFollowMotorID = 4;
-  int kArmSwivelMotor = 5;
-
-  int kMotorCurrentLimit = 50;
-
-  AbsoluteEncoder m_armEncoder; 
-  SparkMaxPIDController m_armPIDController;
-
-
+  //---------------------------
   double startTime;
   double periodicTime;
 
 
+  private XboxController driveController = new XboxController(1);
+  private XboxController opController = new XboxController(0);
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private final DigitalInput SWTCH = new DigitalInput(0);
+
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    
-    m_leftLeadDriveMotor = new CANSparkMax(kLeftLeadMotorID, MotorType.kBrushless);
-    m_rightLeadDriveMotor = new CANSparkMax(kRightLeadMotorID, MotorType.kBrushless);
-    m_leftFollowDriveMotor = new CANSparkMax(kLeftFollowMotorID, MotorType.kBrushless);
-    m_rightFollowDriveMotor = new CANSparkMax(kRightFollowMotorID, MotorType.kBrushless);
-    m_ArmSwivelMotor = new CANSparkMax(kArmSwivelMotor, MotorType.kBrushed);
 
-    m_leftFollowDriveMotor.restoreFactoryDefaults();
-    m_leftLeadDriveMotor.restoreFactoryDefaults();
-    m_rightFollowDriveMotor.restoreFactoryDefaults();
-    m_rightLeadDriveMotor.restoreFactoryDefaults();
-    m_ArmSwivelMotor.restoreFactoryDefaults();
+    M_L1 = new CANSparkMax(L1Id, MotorType.kBrushless);
+    M_R1 = new CANSparkMax(R1Id, MotorType.kBrushless);
+    M_L2 = new CANSparkMax(L2Id, MotorType.kBrushless);
+    M_R2 = new CANSparkMax(R2Id, MotorType.kBrushless);
+    M_ARM = new CANSparkMax(ARMId, MotorType.kBrushed);
+    M_FLNCH = new CANSparkMax(FRLLId, MotorType.kBrushless);
+    M_BLNCH = new CANSparkMax(BRLLId, MotorType.kBrushless);
 
-    m_leftLeadDriveMotor.setIdleMode(IdleMode.kCoast);
-    m_rightFollowDriveMotor.setIdleMode(IdleMode.kCoast);
-    m_ArmSwivelMotor.setIdleMode(IdleMode.kBrake);
 
-    m_leftLeadDriveMotor.setSmartCurrentLimit(kMotorCurrentLimit);
-    m_rightLeadDriveMotor.setSmartCurrentLimit(kMotorCurrentLimit);
-    m_leftFollowDriveMotor.setSmartCurrentLimit(kMotorCurrentLimit);
-    m_rightFollowDriveMotor.setSmartCurrentLimit(kMotorCurrentLimit);
-    m_ArmSwivelMotor.setSmartCurrentLimit(kMotorCurrentLimit);
+    M_L1.restoreFactoryDefaults();
+    M_L2.restoreFactoryDefaults();
+    M_R1.restoreFactoryDefaults();
+    M_R2.restoreFactoryDefaults();
+    M_ARM.restoreFactoryDefaults();
+    M_ARM2.restoreFactoryDefaults();
+    M_FLNCH.restoreFactoryDefaults();
+    M_BLNCH.restoreFactoryDefaults();
 
-    m_leftFollowDriveMotor.follow(m_leftLeadDriveMotor);
-    m_rightFollowDriveMotor.follow(m_rightLeadDriveMotor);
+    M_L2.follow(M_L1);
+    M_R2.follow(M_R1);
+    M_ARM2.follow(M_ARM);
 
-    m_rightLeadDriveMotor.setInverted(false);
-    m_leftLeadDriveMotor.setInverted(true);
-    m_ArmSwivelMotor.setInverted(false);
+    M_L1.setIdleMode(IdleMode.kCoast);
+    M_R2.setIdleMode(IdleMode.kCoast);
+    M_ARM.setIdleMode(IdleMode.kBrake);
+    M_ARM2.setIdleMode(IdleMode.kBrake);
+    M_FLNCH.setIdleMode(IdleMode.kBrake);
+    M_BLNCH.setIdleMode(IdleMode.kBrake);
 
-    m_armPIDController = m_ArmSwivelMotor.getPIDController();
-    m_armEncoder = m_ArmSwivelMotor.getAbsoluteEncoder(Type.kDutyCycle);
-    m_armPIDController.setFeedbackDevice(m_armEncoder);
+    M_L1.setSmartCurrentLimit(CurrentLimit);
+    M_R1.setSmartCurrentLimit(CurrentLimit);
+    M_L2.setSmartCurrentLimit(CurrentLimit);
+    M_R2.setSmartCurrentLimit(CurrentLimit);
+    M_ARM.setSmartCurrentLimit(CurrentLimit);
+    M_ARM2.setSmartCurrentLimit(CurrentLimit);
+    M_FLNCH.setSmartCurrentLimit(CurrentLimit);
+    M_BLNCH.setSmartCurrentLimit(CurrentLimit);
 
-    m_armEncoder.setPositionConversionFactor(360);
-    m_armEncoder.setVelocityConversionFactor(6.0);
-    m_armEncoder.setZeroOffset(kArmOffset);
+    M_L1.setInverted(false);
+    M_R1.setInverted(true);
+    M_ARM.setInverted(false);
+    M_ARM2.setInverted(true);
+    M_FLNCH.setInverted(true);
+    M_BLNCH.setInverted(true);
+ 
+    //armpid------------------------------------------------------------
+     m_armPIDController = M_ARM.getPIDController();
+     m_armEncoder = M_ARM.getAbsoluteEncoder(Type.kDutyCycle);
+     m_armPIDController.setFeedbackDevice(m_armEncoder);
+     m_armEncoder.setPositionConversionFactor(360);
+     m_armEncoder.setVelocityConversionFactor(4.0);
+     m_armEncoder.setZeroOffset(kArmOffset);
+    //--------------------------------------------------------------
+
+    //backrollpid---------------------
+     m_armPIDController = M_BLNCH.getPIDController();
+     backroll = M_BLNCH.getAbsoluteEncoder(Type.kDutyCycle);
+     m_armPIDController.setFeedbackDevice(frontroll);
+     frontroll.setPositionConversionFactor(360);
+     frontroll.setVelocityConversionFactor(5.0);
+     frontroll.setZeroOffset(offset);
+    //---------------------------------------------------
+
+    //frontrollpid---------------------------------
+     m_armPIDController = M_FLNCH.getPIDController();
+     backroll = M_FLNCH.getAbsoluteEncoder(Type.kDutyCycle);
+     m_armPIDController.setFeedbackDevice(backroll);
+     backroll.setPositionConversionFactor(360);
+     backroll.setVelocityConversionFactor(5.0);
+     backroll.setZeroOffset(offset);
+    //----------------------------------------------------
+
+
 
     m_armPIDController.setPositionPIDWrappingEnabled(false);
     m_armPIDController.setPositionPIDWrappingMinInput(kArmMinAngle);
     m_armPIDController.setPositionPIDWrappingMaxInput(kArmMaxAngle);
 
-
     m_armPIDController.setP(Position);
-    //m_armPIDController.setI(Iposition);
     m_armPIDController.setD(Dposition);
-    //m_armPIDController.setFF(FF);
 
-/*claw configuration */
-    m_clawSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, kClawForward, kClawReverse);
+    /* burn flash to store settings */
+    M_L1.burnFlash();
+    M_L2.burnFlash();
+    M_R1.burnFlash();
+    M_R2.burnFlash();
+    M_ARM.burnFlash();
+    M_ARM2.burnFlash();
+    M_FLNCH.burnFlash();
+    M_BLNCH.burnFlash();
 
-/*burn flash to store settings */
-    m_leftLeadDriveMotor.burnFlash();
-    m_rightLeadDriveMotor.burnFlash();
-    m_leftFollowDriveMotor.burnFlash();
-    m_rightFollowDriveMotor.burnFlash();
-    m_ArmSwivelMotor.burnFlash();
-
-    m_myRobot = new DifferentialDrive(m_leftLeadDriveMotor, m_rightLeadDriveMotor);
+    Helios = new DifferentialDrive(M_L1, M_R1);
 
 
-    
   }
-  
-  @Override
-  public void robotPeriodic() {}
 
-  /** This function is run once each time the robot enters autonomous mode. */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /* This runs when robot enters autonomous mode. */
   @Override
   public void autonomousInit() {
     startTime = Timer.getFPGATimestamp();
     m_autoSelected = m_chooser.getSelected();
     // m_autoselected = smartdashboard.getsring("Auto selector", kDefaultAuto);
-    
+
   }
-  
 
   @Override
   public void autonomousPeriodic() {
     if (m_autoSelected.equals(kDefaultAuto)) {
-      if((periodicTime-startTime>0) && (periodicTime-startTime<2)){
-        m_armPIDController.setReference(kArmHoriz, ControlType.kPosition);
-      }else if((periodicTime-startTime>2) && (periodicTime-startTime<8)){
-        m_myRobot.tankDrive(.4, .4);
+      if ((periodicTime - startTime > 0) && (periodicTime - startTime < 2)) {
+        Helios.tankDrive(.4,.4);
+      } else if ((periodicTime - startTime > 2) && (periodicTime - startTime < 10)) {
+       Helios.tankDrive(.0,.5);
       }
-
-    } 
-    
-    else if (m_autoSelected.equals(kCustomAuto)) {
-      if((periodicTime-startTime>0) && (periodicTime-startTime<2)){
-        m_armPIDController.setReference(kArmSlope, ControlType.kPosition);
-        m_clawSolenoid.set(Value.kForward);
-      }else if((periodicTime-startTime>2) && (periodicTime-startTime<5)){
-        m_myRobot.tankDrive(.6, .6);
+      else if ((periodicTime - startTime < 7) && (periodicTime - startTime > 10)){
+      m_armPIDController.setReference(AMP, Controltype.kPosition)
+      }
+      else if ((periodicTime - startTime < 12) && (periodicTime - startTime > 9)){
+        
       }
     }
-   periodicTime = Timer.getFPGATimestamp();
-    
+
+    else if (m_autoSelected.equals(kCustomAuto)) {
+      if ((periodicTime - startTime > 0) && (periodicTime - startTime < 1)) {
+        Helios.tankDrive(.4,.4);
+      } else if ((periodicTime - startTime > 2) && (periodicTime - startTime < 5)) {
+        Helios.tankDrive(.0,.6);
+      }
+      else if ((periodicTime - startTime < 7) && (periodicTime - startTime > 10)) {
+        m_armPIDController.setReference(Speeker, Controltype.kPosition)
+      }
+      else if ((periodicTime - startTime < 9) && (periodicTime - startTime > 12)) {
+        Backrollpid.setReference(SUCk, Controltype.kPosition);
+      }
+      else if (()) {
+
+      }
+    }
+    periodicTime = Timer.getFPGATimestamp();
+
   }
 
   @Override
-  public void teleopInit() {}
-
+  public void teleopInit() {
+  }
 
   @Override
   public void teleopPeriodic() {
-      /*drive */
-    m_myRobot.tankDrive(-opController.getRightY(), -opController.getLeftY());
+
+    Helios.tankDrive(-driveController.getRightY(), -driveController.getLeftY());
     
-  
-    /* claw */
-    if (opController.getBButtonPressed()) { 
-      m_armPIDController.setReference(kArmStow, ControlType.kPosition); 
+   //backrollers------------------------------------
+      double opValue = opController.getRightY();
+      double motorSpeed = opValue;
+      M_BLNCH.set(motorSpeed);
+
+      if (opController.getRightBumperPressed()) {
+      Backrollpid.setReference(SUCk, ControlType.kPosition);
     }
-    if (opController.getYButtonPressed()) { 
-      m_armPIDController.setReference(kArmSlope, ControlType.kPosition); 
+      if (opController.getRightTriggerAxis() > .5) {
+      Backrollpid.setReference(low, ControlType.kPosition);
     }
-    if (opController.getXButtonPressed()) { 
-      m_armPIDController.setReference(kArmHoriz, ControlType.kPosition); 
+    
+    //-------------------------------------------
+
+    //front rollers----------------------------
+
+      double opValue2 = opController.getLeftY();
+      double motorSpeed2= opValue2;
+      M_BLNCH.set(motorSpeed2);
+
+      if (opController.getLeftBumperPressed()) {
+      Frontrollpid.setReference(SUCk, ControlType.kPosition);
+      }
+      if (opController.getLeftTriggerAxis() > .5) {
+      Backrollpid.setReference(psh, ControlType.kPosition);
     }
-    if (opController.getAButtonPressed()) { 
-      m_armPIDController.setReference(kArmGround, ControlType.kPosition); 
-    }
-    if (opController.getRightBumper()) { 
-      m_armPIDController.setReference(kArmDub, ControlType.kPosition); 
-    }
+    //---------------------------------------------------
 
     /* arm */
-    if (opController.getRightTriggerAxis() > .5 ) { m_clawSolenoid.set(Value.kForward); }
-    if (opController.getLeftTriggerAxis() > .5 ) { m_clawSolenoid.set(Value.kReverse); }
 
-    SmartDashboard.putNumber("Encoder Value", m_armEncoder.getPosition());
+    if (opController.getBButtonPressed()) {
+      m_armPIDController.setReference(STW, ControlType.kPosition);
+    }
+    if (opController.getYButtonPressed()) {
+      m_armPIDController.setReference(SCOOP, ControlType.kPosition);
+    }
+    if (opController.getXButtonPressed()) {
+      m_armPIDController.setReference(AMP, ControlType.kPosition);
+    }
+    if (opController.getAButtonPressed()) {
+      m_armPIDController.setReference(Speeker, ControlType.kPosition);
+    }
+
+    SmartDashboard.getBoolean("SWTCH", false);
+
+    SWTCH.get();
+    SmartDashboard.putBoolean("SWTCH", SWTCH.get());
+
+
+    SmartDashboard.putNumber("arm Value", m_armEncoder.getPosition());
+    SmartDashboard.putNumber("back Value", backroll.getPosition());
+    SmartDashboard.putNumber("front Value", frontroll.getPosition());
   }
 }
+
+ 
